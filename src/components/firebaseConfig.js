5ddd13ba64b48fa93d3813c7002267b6a9d3d24e
Xuananh2000 <xuanna@kaiyouit.com>
2023-03-13 22:30:24 +0700

    [main][Add]-Add base

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAfeRSpWGFsayOUPGdX1W4TTn16qwoOnnE",
+  authDomain: "vuefs-prod-472cd.firebaseapp.com",
+  projectId: "vuefs-prod-472cd",
+  storageBucket: "vuefs-prod-472cd.appspot.com",
+  messagingSenderId: "373025353719",
+  appId: "1:373025353719:web:912ba62fd1f2ef47df88f2",
+  measurementId: "G-4PQ5XKMC3N"
+}
